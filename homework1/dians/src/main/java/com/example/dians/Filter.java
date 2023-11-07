dd858f50650d6eb6a3f32004fc75f7c154c2a003
Jonatan Mileski <mileskijonatan2@gmail.com>
2023-11-07 22:14:30 +0100

    data reading and pipe and filter classes added

---
+++ homework1/dians/src/main/java/com/example/dians/Filter.java
@@ -0,0 +1,5 @@
+package com.example.dians;
+
+public interface Filter <T>{
+    T execute(T input);
+}
---
+++ homework1/dians/src/test/java/com/example/dians/DiansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dians;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
